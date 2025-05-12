820c8a6bfe119bc0f6632e5568f6b130034b37d0
Elise Aithocine <eaithocine@globusmedical.com>
2025-05-12 15:25:47 +0200

    DEVOPS-1057 updating script and cmakeLists with SOUP path and variables

--- spix_build_qt5.sh
+++ spix_build_qt5.sh
@@ -5,11 +5,12 @@
 working_dir=$(pwd)
 echo "pwd: $working_dir"
 anyrpc_7z="./anyrpc-rel.7z"
-Qt5_DIR="C:/Qt/5.15.8/msvc2019_64/lib/cmake/Qt5" # Change this to your Qt5_DIR
+Qt5_DIR="D:/Development/gm-venv/sdk-cpp/qt-v5-x64_5.15.18-gm001-rev_42f2de5/lib/cmake/Qt5" # Change this to your Qt5_DIR
+PATH=$PATH:"D:\Development\gm-venv\sdk-cpp\ninja-x64_1.12.1-rev_81ba1f1\bin"
 rm Spix*.lib
 cp CMakeLists.qt5 CMakeLists.txt
 [ -d $working_dir/anyrpc ] && rm -r $working_dir/anyrpc
-"/c/Program Files/7-Zip/7z.exe" x $anyrpc_7z -o$working_dir
+"/c/Program Files (x86)/7-Zip/7z.exe" x $anyrpc_7z -o$working_dir
 [ -d build ] && rm -r build
 mkdir build && cd build
 build_type="Debug"
